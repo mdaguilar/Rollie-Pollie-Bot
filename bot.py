4b31c445a000c58a630ccdbe58f259baf42fbf07
mdaguilar <46389888+mdaguilar@users.noreply.github.com>
2024-01-23 20:16:52 -0800

    Add Command: alignment
    Added a !alignment command, where you can learn more about the different types of alignments your character can pick from.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import discord
 import random
+import requests
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -10,6 +11,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 print("running")
+
 
 @bot.command(name="roll", help="Simuates a die roll depending on the number of sides you enter. You can also enter how many of that die you'd like to roll.")
 async def roll(ctx, numSides, numToRoll=1):
@@ -23,5 +25,24 @@ async def roll(ctx, numSides, numToRoll=1):
     except Exception as e:
         print(f"Error: {e}")
 
+@bot.command(name="alignment", help="Get info on the different alignments by entering its abreviation.")
+async def roll(ctx, alignmentAbbreviation=""):
+    print("COMMAND: alignment")
+    if alignmentAbbreviation == "":
+        await ctx.send("**Choose from the following abreviations**: LG, NG, CG, LN, N, CN, LE, NE, CE")
+
+    alignmentAbbreviation = alignmentAbbreviation.upper()
+    indexConversion = {
+        "LG":"lawful-good", "NG":"neutral-good", "CG":"chaotic-good",
+        "LN":"lawful-neutral", "N":"neutral", "CN":"chaotic-neutral",
+        "LE":"lawful-evil", "NE":"neutral-evil", "CE":"chaotic-evil",
+        }
+        
+    try:
+        response = requests.get(f"https://www.dnd5eapi.co/api/alignments/{indexConversion[alignmentAbbreviation]}")
+        result = f'**Alignment: {response.json()["name"]}**\n> {response.json()["desc"]}'
+        await ctx.send(result)
+    except Exception as e:
+        print(f"Error: {e}")
 
 bot.run(TOKEN)
